8b49c2e38312eb6b71e0723d442ab0f30b74e259
PeterZhizhin <piter.zh@gmail.com>
2014-05-08 23:49:54 +0600

    Prepared settings. Need to add info about variables.

--- src/live/wallpaper/Settings.java
+++ src/live/wallpaper/Settings.java
@@ -3,12 +3,18 @@ package live.wallpaper;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 public class Settings extends PreferenceActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+    }
+
+    private class Preference extends PreferenceFragment
+    {
 
     }
 }
