6d88ac44b5fe375e46b190b40ce961ae91ec8678
PeterZhizhin <piter.zh@gmail.com>
2014-05-08 20:12:09 +0600

    Settings

---
+++ src/live/wallpaper/Settings.java
@@ -0,0 +1,16 @@
+package live.wallpaper;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by notebook on 08.05.14.
+ */
+public class Settings extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+}
