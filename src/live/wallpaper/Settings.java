aa705031d7c506c72fe4b4462a8abed2e3404a69
PeterZhizhin <piter.zh@gmail.com>
2014-05-23 22:09:24 +0600

    Добавлена реклама в настройки

--- src/live/wallpaper/Settings.java
+++ src/live/wallpaper/Settings.java
@@ -1,10 +1,21 @@
 package live.wallpaper;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import live.wallpaper.Ads.AdBuilder;
 
 public class Settings extends PreferenceActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -12,6 +23,7 @@ public class Settings extends PreferenceActivity {
         Preference preference = new Preference();
         getFragmentManager().beginTransaction().replace(android.R.id.content, preference).commit();
     }
+
 
     @Override
     public void onPause()
@@ -33,6 +45,13 @@ public class Settings extends PreferenceActivity {
         {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundleSavedInstance) {
+            LinearLayout linearLayout = (LinearLayout) super.onCreateView(inflater, container, bundleSavedInstance);
+            linearLayout.addView(AdBuilder.createAdView(getBaseContext(), AdBuilder.bannerAtSettingsID));
+            return linearLayout;
         }
     }
 }
