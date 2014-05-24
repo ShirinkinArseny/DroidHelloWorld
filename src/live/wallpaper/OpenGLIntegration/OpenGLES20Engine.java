628b3730f2ff5c14aad56b84ed9185ae28cb038c
PeterZhizhin <piter.zh@gmail.com>
2014-05-24 23:29:44 +0600

    Merge branch 'master' of /home/peter/Projects/Java/DroidHelloWorld with conflicts.

--- src/live/wallpaper/OpenGLIntegration/OpenGLES20Engine.java
+++ src/live/wallpaper/OpenGLIntegration/OpenGLES20Engine.java
@@ -10,9 +10,14 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 
+import static android.opengl.GLES20.*;
+
 public class OpenGLES20Engine implements GraphicEngine {
 
+    //В float 4 байта
     private static final int BYTES_PER_FLOAT = 4;
+    //Вершины двухмерные
+    private static final int POSITION_COMPONENT_COUNT = 2;
 
     /**
      * Метод, создающий нативный массив значений float из массива Java
@@ -73,7 +78,39 @@ public class OpenGLES20Engine implements GraphicEngine {
 
     @Override
     public void drawRect(float x, float y, float x2, float y2, float r, float g, float b, float a) {
-
+        float sizeX = x2-x;
+        float sizeY = y2-y;
+        //Создаем массив вершин
+        FloatBuffer nativeVertexes = createNativeFloatArray(new float[]{
+                //Верхний левый угол
+                x, y2,
+                //Верхний правый угол
+                x2,y2,
+                //Нижний правый угол
+                x2,y,
+                //Нижний левый угол
+                x,y
+        });
+        //Используем программу простой заливки
+        glUseProgram(fillColorShader);
+        final int aPosition = glGetAttribLocation(fillColorShader, "a_Position");
+        //Устанавливаем указатель на начало массива
+        nativeVertexes.position(0);
+        //Задаем алгоритм обхода вершин в массиве
+        glVertexAttribPointer(
+                //Атрибут задан
+                aPosition,
+                //Количество компонент в вершине равно 2 (2D)
+                POSITION_COMPONENT_COUNT,
+                //Все значения имеют тип float
+                GL_FLOAT,
+                //Значения не должны быть нормализированы
+                false,
+                //После вершины пробел для каких-либо ещё величин в массиве делать не нужно т.к. их нет
+                0,
+                //Все данные о вершинах лежат в nativeVertexes
+                nativeVertexes);
+        //Заюзали
     }
 
 }
