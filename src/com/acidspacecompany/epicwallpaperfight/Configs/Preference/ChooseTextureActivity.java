0b279b188de04cdd9d19c0f6655b9d8655342bd3
PeterZhizhin <piter.zh@gmail.com>
2014-07-09 00:11:37 +0600

    Animation fixed!

--- src/com/acidspacecompany/epicwallpaperfight/Configs/Preference/ChooseTextureActivity.java
+++ src/com/acidspacecompany/epicwallpaperfight/Configs/Preference/ChooseTextureActivity.java
@@ -1,17 +1,12 @@
 package com.acidspacecompany.epicwallpaperfight.Configs.Preference;
 
-import android.animation.ArgbEvaluator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.*;
-import android.graphics.drawable.BitmapDrawable;
-import android.hardware.display.DisplayManager;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +14,7 @@ import android.widget.*;
 import com.acidspacecompany.epicwallpaperfight.Configs.LocalConfigs;
 import com.acidspacecompany.epicwallpaperfight.R;
 
-public class ChooseTextureActivity extends Activity implements AdapterView.OnItemClickListener {
+public class ChooseTextureActivity extends Activity implements AbsListView.OnScrollListener {
     //Массив id картинок
     private static final int[] images = new int[] {
             R.drawable.grid, R.drawable.tex2,
@@ -45,7 +40,8 @@ public class ChooseTextureActivity extends Activity implements AdapterView.OnIte
         gridview.setBackgroundColor(colorBackground);
 
         gridview.setAdapter(new ImageAdapter(this));
-        gridview.setOnItemClickListener(this);
+
+        gridview.setOnScrollListener(this);
 
     }
 
@@ -53,8 +49,23 @@ public class ChooseTextureActivity extends Activity implements AdapterView.OnIte
     int colorBorder;
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
     }
+
+    private static boolean isScrolled = false;
+    private static void setScrollListen() {
+        isScrolled = false;
+    }
+    private static boolean getIsScrolled() {
+        return isScrolled;
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        isScrolled = true;
+    }
+
 
     private class ImageAdapter extends BaseAdapter {
         private int getLandDiv2() {
@@ -95,50 +106,83 @@ public class ChooseTextureActivity extends Activity implements AdapterView.OnIte
                 imageView.setLayoutParams(new GridView.LayoutParams(size, size));
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 imageView.setBackgroundColor(colorBorder);
-                imageView.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(final View v, MotionEvent event) {
-                        if (event.getAction() == MotionEvent.ACTION_DOWN)
-                        {
-                            ValueAnimator valueAnimator = ValueAnimator.ofFloat(0,1);
-                            valueAnimator.setDuration(100);
-                            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                                @Override
-                                public void onAnimationUpdate(ValueAnimator animation) {
-                                    final int aB = Color.alpha(colorBackground);
-                                    final int rB = Color.red(colorBackground);
-                                    final int gB = Color.green(colorBackground);
-                                    final int bB = Color.blue(colorBackground);
-
-                                    final int aE = Color.alpha(colorBorder);
-                                    final int rE = Color.red(colorBorder);
-                                    final int gE = Color.green(colorBorder);
-                                    final int bE = Color.blue(colorBorder);
-
-                                    final float part = (float)animation.getAnimatedValue();
-
-                                    final int aD = (int) (aB * part + aE * (1 - part));
-                                    final int rD = (int) (rB * part + rE * (1 - part));
-                                    final int gD = (int) (gB * part + gE * (1 - part));
-                                    final int bD = (int) (bB * part + bE * (1 - part));
-
-                                    v.setBackgroundColor(Color.argb(aD, rD, gD, bD));
-                                }
-                            });
-                            valueAnimator.start();
-                        }
-                        else if (event.getAction() == MotionEvent.ACTION_UP) {
-                            Toast.makeText(ChooseTextureActivity.this, "Texture " + position, Toast.LENGTH_SHORT).show();
-                        }
-                        return true;
-                    }
-                });
+                imageView.setOnTouchListener(new TouchListener(position));
             }
             else {
                 imageView = (ImageView)convertView;
             }
             imageView.setImageResource(images[position]);
             return imageView;
+        }
+
+
+         public class TouchListener implements View.OnTouchListener {
+            private int position;
+            private ValueAnimator animator, valueAnimator;
+
+            public TouchListener(int position) {
+                this.position = position;
+            }
+
+
+            @Override
+            public boolean onTouch(final View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        valueAnimator = ValueAnimator.ofFloat(0,1);
+                        valueAnimator.setDuration(100);
+                        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                animate(animation, v);
+                            }
+                        });
+                        valueAnimator.start();
+                        setScrollListen();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        if (getIsScrolled()) {
+                            animator = ValueAnimator.ofFloat(1,0);
+                            animator.setDuration(100);
+                            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                @Override
+                                public void onAnimationUpdate(ValueAnimator animation) {
+                                    animate(animation, v);
+                                }
+                            });
+                            animator.start();
+                        }
+                        else {
+                            Toast.makeText(ChooseTextureActivity.this, "Texture " + position, 1).show();
+                        }
+                        break;
+                }
+                return true;
+            }
+
+            public void animate(ValueAnimator animation, View v) {
+                final int aB = Color.alpha(colorBackground);
+                final int rB = Color.red(colorBackground);
+                final int gB = Color.green(colorBackground);
+                final int bB = Color.blue(colorBackground);
+
+                final int aE = Color.alpha(colorBorder);
+                final int rE = Color.red(colorBorder);
+                final int gE = Color.green(colorBorder);
+                final int bE = Color.blue(colorBorder);
+
+                final float part = (float)animation.getAnimatedValue();
+
+                final int aD = (int) (aB * part + aE * (1 - part));
+                final int rD = (int) (rB * part + rE * (1 - part));
+                final int gD = (int) (gB * part + gE * (1 - part));
+                final int bD = (int) (bB * part + bE * (1 - part));
+
+                v.setBackgroundColor(Color.argb(aD, rD, gD, bD));
+            }
+
+
         }
     }
 }
